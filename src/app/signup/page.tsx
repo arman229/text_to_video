45b25ecbc073eded1503de47e418cc757a84c68b
arman229 <armanashraf015@gmail.com>
2024-06-02 08:06:41 -0700

    update my code

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -7,8 +7,16 @@ import {
   DialogContent,
   DialogDescription,
   DialogFooter,
-  DialogHeader,
+  DialogHeader,   
+
+
+
+
+
   DialogTitle,
+
+
+  
   DialogTrigger,
 } from "@/components/ui/dialog"
 import { Input } from "@/components/ui/input"
